6a886ca3fb320dfc8fd4c0f77c4a47b3ee5cfb3d
poyrazavsever <poyrazavsever@hotmail.com>
2026-07-19 00:37:45 +0300

    Refresh hero with video background

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <SiteHeader />
-      <main className="flex min-h-screen flex-col pt-24">
+      <main className="flex min-h-screen flex-col">
         {/* Hero Section */}
         <HeroSection />
 
